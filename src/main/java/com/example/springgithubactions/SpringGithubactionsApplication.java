fbfefc60eb64ba37b6666a0310ed18ab434ba8e3
Komichi Soudai <2101122@s.asojuku.ac.jp>
2024-02-06 22:09:37 +0900

    初期コミット

---
+++ src/main/java/com/example/springgithubactions/SpringGithubactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springgithubactions/SpringGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
